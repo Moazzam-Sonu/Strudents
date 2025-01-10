2dc0513485dfe2c0352c429bf3fdccd565e04836
MoazzamShabbir <MoazzamShabbir@MOAZZAM>
2025-01-10 11:08:56 +0500

    Add project files.

---
+++ StrudentCurd/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿namespace StrudentCurd.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<StrudentCurd.Model.Stuent> Stuent { get; set; }
+    }
+}
---
+++ StrudentCurd/Model/Stuent.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StrudentCurd.Model
+{
+    public class Stuent
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string RollNo { get; set; }
+        public string Course { get; set; }
+    }
+}
